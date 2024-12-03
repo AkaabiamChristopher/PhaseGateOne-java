554cdfdffd6e2192b3a5459b7347c60f93151e2e
AkaabiamChristopher <chrisakaabiam@gmail.com>
2024-12-03 10:53:05 +0100

    origin

---
+++ StudentGrade.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class StudentGrade{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter number of student:");
+int numberOfStident = scanner.nextInt();
+
+System.out.print("Enter number of subjects:");
+int numberOfSubject = scanner.nextInt();
+
+int [][] studenGrade = new int[numberOfStident][ numberOfSubject];
+ int student;
+ int subject;
+for(int sudent = 0;numberOfStident;count++){
+
+	for(int subject = 0; subject < numberOfStident; subjec++){
+	
+
+	}
+}
+
+
+
+
+
+     }
+}
