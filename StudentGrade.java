284025c901e0bce524a06c30773038343d49aba2
AkaabiamChristopher <chrisakaabiam@gmail.com>
2024-12-06 09:15:05 +0100

    origin

--- StudentGrade.java
+++ StudentGrade.java
@@ -15,7 +15,7 @@ public class StudentGrade {
      int[][] scores = new int[numberofstudent][numberofsubject];
 
      for (int count = 0; count < numberofstudent; count++) {
-        System.out.println("Enter scores for student " + (count + 1) + ":");
+        System.out.println("Enter student score " + (count + 1) + ":");
            for (int i = 0; i < numberofsubject; i++) {
              while (true) {
              System.out.print("Subject " + (i + 1) + ": ");
@@ -26,7 +26,8 @@ public class StudentGrade {
 		break;
 
                     } else {
-                        System.out.println("Scores must be between 0 and 100. Enter again.");
+                        System.out.println("Scores must be between from 0 to 100. Enter scores again.");
+
                     }
                 }
             }
@@ -40,12 +41,50 @@ public class StudentGrade {
         }
 System.out.println("==========================================================");
 
-System.out.println("STUDENT  SUB1 SUB2 SUB3 TOT AVE POS");
+
+ System.out.print("         ");
+    for (int sub = 0; sub < numberofsubject; sub++) {
+            System.out.print("SUB " + (sub + 1) + "   ");
+                        }
+           System.out.print("TOT   AVE    POS");
+            System.out.println();
+
 
 System.out.println("==========================================================");
 
 
-         }
+        for (int i = 0; i < numberofstudent; i++) {
+            int totalScore = 0;
+            System.out.print("student " + (i + 1) + ": ");
+            for (int j = 0; j < numberofsubject; j++) {
+                System.out.print(scores[i][j] + "     ");
+                totalScore += scores[i][j];
+            }
+            
+
+	double averageScore = (double) totalScore / numberofsubject;
+
+            System.out.println("  " +   totalScore +   "   " + averageScore);
+
+        }
+
+
+System.out.println("==========================================================");
+System.out.println("==========================================================");
+
+System.out.println("SUBJECT SUMMARY");	
+ for (int subject= 0; subject < numberofsubject; subject++) {
+            System.out.print("Subject " + (subject + 1) + "");
+			System.out.println();
+			   System.out.println("Highest scoring student is:");
+			   System.out.println("Lowest scoring student is:");
+
+				
+                        }
+          
+
+
+    }
 }
 
     
